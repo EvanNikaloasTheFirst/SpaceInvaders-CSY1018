f2c2b51346cccaa66b1a4fd4c3c1be94ba020a25
EvanNikaloasTheFirst <evangelosniko@outlook.com>
2022-06-09 14:51:16 +0100

    Best version so far

--- game.js
+++ game.js
@@ -9,7 +9,7 @@ var level = 3;
 var finalScore;
 var count = 0;
 
-var shot = true;
+var stopPlaying = false;
 
 
 //if this function remains true it will allows the functions regarding hte bombs , spaceship and general game flow to operate as normal
@@ -17,9 +17,14 @@ var DropBombs = true;
 
 var scoresCount = true;
 
+if (DropBombs == true)
+{
+	
 
 function keyup(event) {
 	var player = document.getElementById('player');
+	if ( DropBombs == true)
+	{
 	if (event.keyCode == 37) {
 		leftPressed = false;
 		lastPressed = 'left';
@@ -35,6 +40,7 @@ function keyup(event) {
 	if (event.keyCode == 40) {
 		downPressed = false;
 		lastPressed = 'down';
+		
 	}
 	// if spacebar is clicked change player animation
 	if (event.keyCode == 32)
@@ -43,9 +49,10 @@ function keyup(event) {
 		lastPressed = 'up fire';
 		
 	}
-
+	// console.log(' top:' +player.offsetTop, 'left:' + player.offsetLeft)
 	player.className = 'character stand ' + lastPressed;
 
+}
 }
 
 
@@ -53,6 +60,10 @@ function move() {
 	var player = document.getElementById('player');
 	var positionLeft = player.offsetLeft;
 	var positionTop = player.offsetTop;
+	if (DropBombs == true)
+	{
+
+	
 	if (downPressed) {
 		var newTop = positionTop+1;
 
@@ -103,56 +114,62 @@ function move() {
 		player.className = 'character walk right';
 	}
 
-	if (spaceBarPressed && DropBombs == true) 
+	if (spaceBarPressed) 
 	{
-		
-		
+		var sky = document.getElementsByClassName('sky')[0];
+	
+		var anyNumber = Math.ceil(Math.random() * 900);
+		var bowTop = 700;
+		var bowLeft =  anyNumber;
+		var bombs = document.getElementsByClassName('bomb');
+		var bow = document.getElementsByClassName('arrow');
+
+		var player = document.getElementsByClassName('player')
 		
 
-		var sky = document.getElementsByClassName('sky')[0];
-		var skyHeight = sky.offsetHeight;
-		var bowTop = 1000;
-		var bowLeft =  1000;
 		
 //Once spacebar is clicked Arrow is generated
-
-
 /* sourced from overflow.com/questions/27645619/javascript-on-key-press-trigger-only-once */
 		document.body.onkeydown  = function(e){
 		if(e.keyCode == 32){
 		var bow = document.createElement('div');
 		bow.classList = 'arrow';
+		
 		// bombs move upwards 
-		setInterval(function(){	
-		var topLeftBow = document.elementFromPoint(bow.offsetLeft,bow.offsetTop);
+		var shootBows = setInterval(function(){	
+
 		bow.style.top = bowTop  + 'px' ;
 		bow.style.left = bowLeft  + 'px';
-
-		bow.style.transform = 'rotate(-90deg)';
+		// bow.style.transform = 'rotate(-90deg)';
 		console.log('Arrow created');
 		
-		document.body.appendChild(bow)
-		if (topLeftBow.classList.contains('bomb')== true || bowTop == 0)
-		{
-			bow.parentNode.removeChild(bow);
-		}
-		else
-			 {
+		document.body.appendChild(bow);
+		
+		
+			{
 				bowTop = bowTop - 1;
 				bow.style.top = bowTop - 'px';
+				
+		console.log(bowTop)
+		
 
+				if (bowTop < 0 )
+				{
+// removes the bow onces it leaves the screen
+					bow.parentNode.removeChild(bow);
+					console.log('remove bow')
+					
+				}
 			}
-		},3)
-			
-	
+		
+		},20);
 	}
 }
 	}
 
 }
 
-
-
+}
 
 
 function keydown(event) {
@@ -176,9 +193,12 @@ function keydown(event) {
 }
 // allows the game to 'begin' 
 function startGame(bomb){
+	
 	timeout = setInterval(move,10);
 	document.addEventListener('keydown',keydown);
 	document.addEventListener('keyup',keyup);
+
+	stopPlaying = true;
 	
 	var playerName = document.getElementById('pn');
 
@@ -190,17 +210,20 @@ function startGame(bomb){
 	}
 	playerName.textContent = welcomeText;
 	
+
 	var playGame = document.getElementsByClassName('start')[0];
 	playGame.style.display = 'none';
 	createSpaceShip(1);
 	createMoreSpaceShip()
 	setInterval(spaceShipPositions,2000);
+
+}
 }
 
 
 function createSpaceShip(number){
 
-	if (DropBombs == true)
+	if (stopPlaying == true)
 		{
 	for ( var i = 0; i < number; i++){
 	var spaceShip = document.createElement('div');
@@ -213,7 +236,7 @@ function createSpaceShip(number){
 
 function createMoreSpaceShip(number){
 //Every 35 seconds another Spaceship is added
-if (DropBombs == true)
+if (stopPlaying == true)
 {
 	setInterval(function(){
 		var spaceShip = document.createElement('div');
@@ -241,13 +264,15 @@ function spaceShipPositions (){
 		bombs.style.top = '93px';
 		document.body.appendChild(bombs);
 		moveBomb(bombs);
+		
 		}
 	}
 	
 }
 
 function moveBomb (bomb){
-		
+		if (stopPlaying = true)
+		{
 		var scores = document.getElementsByClassName('scoreboard')[0];
 		var velocity = Math.ceil(Math.random()*10);
 		var moveBombsToTheRight = Math.ceil(Math.random()*10)-9;
@@ -256,15 +281,13 @@ function moveBomb (bomb){
 		var sky = document.getElementsByClassName('sky')[0];
 		var skyHeight = sky.offsetHeight;
 
-	
 		var player = document.getElementById('player');
-	
 		var grass = window.innerHeight - skyHeight;
 		var randomPoint = Math.floor(Math.random() * grass);
-		
-		
+
 		var fall = setInterval(function(){
 			var topLeft = document.elementFromPoint(player.offsetLeft,player.offsetTop);
+
 			if (top - randomPoint > skyHeight)
 		
 			{
@@ -278,14 +301,13 @@ function moveBomb (bomb){
 				player.classList = 'character hit left'
 			}
 			setTimeout(function(){
-				if (bomb.parentNode != null )
+				if (bomb.parentNode != null)
 				{
 					bomb.parentNode.removeChild(bomb);
 					
 					count+= 1;
 					console.log(count)
 					scores.innerHTML = count;
-				
 				}
 			},800)
 			
@@ -296,7 +318,7 @@ function moveBomb (bomb){
 			var distanceToLeft = 0.1
 			top = top + 1
 			bomb.style.top = top + 'px';
-
+			for(var i = 0; i < bomb.length; i++)
 			
 			left = left + distanceToLeft;
 			bomb.style.left = left + 'px';
@@ -307,10 +329,13 @@ function moveBomb (bomb){
 				left = left + distanceToLeft;
 			bomb.style.left = left + 'px';
 			}
+			
 		}
+		
+	
 	},velocity)
 
-	
+}
 }
 
 
@@ -318,6 +343,7 @@ function removeLife(scores,spaceShip)
 {
 	var player = document.getElementsByClassName('player')
 	var life = document.getElementsByTagName('li');
+	
 	
 	
 //if the life is higher than 1 remove a life
@@ -332,9 +358,9 @@ function removeLife(scores,spaceShip)
 	}
 	if (hit > 3)
 	{
-		
-		player.classList = 'character dead';
+		var anyNumber = Math.ceil(Math.random(49) * 51);
 
+		
 		// Created a new button which refreshes the page
 		var btn = document.createElement('button');
 		btn.innerHTML = "GAME OVER , Play Again?";
@@ -342,8 +368,8 @@ function removeLife(scores,spaceShip)
 		btn.style.zIndex = '1000';
 		btn.style.width = 'fit-content';
 		btn.style.backgroundColor = 'red';
-		btn.style.left = '50%';
-		btn.style.top = '50%';
+		btn.style.left = 50 +'vw';
+		btn.style.top = 50 +'vh';
 		btn.style.marginTop = '-1em';
 		btn.style.textAlign = 'center';
 		btn.style.marginLeft = 'auto';
@@ -358,6 +384,7 @@ function removeLife(scores,spaceShip)
 		btn.style.position = 'absolute';
 		document.body.appendChild(btn);
 		DropBombs = false;
+		stopPlaying = false;
 
 
 
@@ -377,6 +404,8 @@ function removeLife(scores,spaceShip)
 
 	
 }	
+
+
 
 
 
